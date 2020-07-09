dc40c2d524c6a8b00d9a4671a9ee999eb2f43885
Michael Cinus <michaelcinus@gmail.com>
2020-07-09 15:10:42 +0200

    HTML & JS: costruzione function coutdown, e diverse comunicazioni.

--- script.js
+++ script.js
@@ -11,36 +11,43 @@ while (j < 5) {
     }
 }
 console.log(numeriRandom);
-
-//CHIEDO all'utente di inserire i 5 numeri 
-var numeriUtente = [];
-for (var i = 0; i < 5; i++) {
-    numeriUtente [i] = parseInt(prompt("Inserisci i numeri"))
-}
-console.log(numeriUtente);
+alert(numeriRandom);
 
 
-//VERIFICO se i numeri inseriti sono corretti, e stabilisco il risultato
-var numeriIndovinati = [];
-var punteggio = 0;
-for (var j = 0; j < 5; j++) {
-    var numero = numeriUtente [j];
-    if (inArray(numeriRandom, numero)) {
-        numeriIndovinati.push(numero);
-        punteggio++;
+
+
+//TIMER e chiusura prompt
+var timeLeft = 5;
+var timerId = setInterval(countdown, 1000);
+
+//FUNCTION coutdown  
+function countdown() {
+    if (timeLeft == 0) {
+    clearTimeout(timerId);
+    alert("tempo finito");
+        //CHIEDO all'utente di inserire i 5 numeri 
+        var numeriUtente = [];
+        for (var i = 0; i < 5; i++) {
+        numeriUtente [i] = parseInt(prompt("Inserisci i numeri"))
+    } 
+    console.log(numeriUtente);
+    //VERIFICO se i numeri inseriti sono corretti, e stabilisco il risultato
+    var numeriIndovinati = [];
+    var punteggio = 0;
+    for (var j = 0; j < 5; j++) {
+        var numero = numeriUtente [j];
+        if (inArray(numeriRandom, numero)) {
+            numeriIndovinati.push(numero);
+            punteggio++;
+        }
+    }
+    document.getElementById("h1").innerHTML = "Hai totalizzato " + punteggio + " punti, indovinando i seguenti numeri: " + numeriIndovinati;
+        console.log(punteggio, numeriIndovinati);
+    } else {
+    document.getElementById("h1").innerHTML = timeLeft;
+    timeLeft--;
     }
 }
-console.log(punteggio, numeriIndovinati);
-
-
-
-
-
-
-
-
-
-
 
 //FUNCTION genera numeri casuali
 function generaNumeroRandom(min, max) {
@@ -62,3 +69,4 @@ function inArray (array, elemento) {
     }
     return trovato;
 }
+
